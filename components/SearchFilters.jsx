8073443757bdd14c32c285b3fa881d3b26d78843
Luiz Gustavo Ferreira <gus.116506@gmail.com>
2024-01-03 16:28:39 -0300

    criating the search filter.

---
+++ components/SearchFilters.jsx
@@ -0,0 +1,20 @@
+import {
+  Flex,
+  Select,
+  Box,
+  Text,
+  Input,
+  Spinner,
+  Icon,
+  Button,
+} from "@chakra-ui/react";
+import { MdCancel } from "react-icons/md";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+
+const SearchFilters = () => {
+  return <div>searchFilters</div>;
+};
+
+export default SearchFilters;
