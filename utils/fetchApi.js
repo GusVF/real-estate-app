fca0871378dbf53a1fe4dda7f62dd8eb70f30305
Luiz Gustavo Ferreira <gus.116506@gmail.com>
2024-01-05 06:17:20 -0300

    .env created. no go on the scroll.

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -6,7 +6,7 @@ export const fetchApi = async (url) => {
   const { data } = await axios.get((url), {
     headers: {
       'X-RapidAPI-Host': 'bayut.p.rapidapi.com',
-      'X-RapidAPI-Key': 'bcb0b50cd1mshc128b86cd332e16p15b495jsn36a6eff1e70c'
+      'x-rapidapi-key':process.env.NEXT_PUBLIC_RAPID_API_KEY,
     }
   });
   return data;
